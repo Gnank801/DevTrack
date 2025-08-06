2713eb917c5fe5520d6cc33951dbc4281a1f777f
Gnank801 <kugn801@gmail.com>
2025-08-06 21:41:23 +0530

    Fix issue_service tests to use in-memory DB

--- issue_service/tests/test_issues.py
+++ issue_service/tests/test_issues.py
@@ -1,7 +1,6 @@
-
 import pytest
 from app import create_app
-from models import db, Issue
+from models import db, Issue, Comment
 from config import Config
 
 class TestingConfig(Config):
@@ -30,4 +29,3 @@ def test_create_issue(client):
     assert response.status_code == 201
     issue = Issue.query.filter_by(title="My First Test Issue").first()
     assert issue is not None
-
